cebc1f6822e58afad41c7539c952985dacfe7f4e
unknown <phongpham29305@gmail.com>
2024-12-19 21:33:58 +0700

    K23CNT1_PhamTuanPhong_2310900081_Project1

---
+++ K23CNT1_PhamTuanPhong_2310900081_Project1/database/seeders/PTP_LOAI_SAN_PHAMTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PTP_LOAI_SAN_PHAMTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        DB::table('PTP_LOAI_SAN_PHAM')->insert([
+            'ptpMaLoai' => 'L001',
+        ]);
+    }
+}
